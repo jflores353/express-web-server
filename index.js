b8da7c1267b0459ff7930f423a0310a7f5f843f0
Gabe <jflores353@gmail.com>
2019-02-10 02:08:54 -0600

    completed Express practice assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 
+const express = require("express");
+const bodyParser = require("body-parser");
+let users = require("./state").users;
+let products = require("./state").products;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/users", (request, response)=>{
+    response.send(users)
+});
+
+app.post("/users", (request, response, next)=>{
+    let newUser = request.body
+    users.push(newUser);
+    response.json(newUser);
+});
+
+app.get("/users/:userId",function (request,response,next){
+    response.json(users[request.params.userId]);
+});
+ 
+// *** Products from state.js ***
+
+app.get("/products", (request, response, next)=>{
+    response.send(products)
+});
+
+app.post("/products", (request, response, next)=>{
+    let newProduct = request.body
+    products.push(newProduct);
+    response.json(newProduct);
+});
+
+app.get("/products/:productId", (request,response,next) => {
+    response.json(products[request.params.productId])
+});
+
+app.listen(3008, (err) => {
+ if (err) {
+   return console.log("Error", err);
+ }
+ console.log("Web server is now listening for messages", err);
+});
+
